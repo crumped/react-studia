530fda3d635a01965abf5471a21a42cced798c8a
MierzejewskiS <slawek321q@wp.pl>
2021-04-26 21:54:34 +0200

    create function to operate with the database

--- database/db.ts
+++ database/db.ts
@@ -1,20 +1,71 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./database/database.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the database.');
-});
+export const OpenDB = () =>
+{
+    let db = new sqlite3.Database('./database/database.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Connected to the database.');
+    });
+    return db;
+}
 
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-});
+export const CloseDB = (db) =>
+{
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+}
 
-// insert('table', 'values1, etc', 'where condition)
-// update
-// select
-// delete
+export const Insert = (table, value, ...args) =>
+{
+    let db = OpenDB();
+
+    db.exec("INSERT INTO ? VALUES(?)", table, value, (err) =>{
+        if(err) {
+            console.error(err.message);
+            console.log("Error in insert to database");
+        }
+    });
+
+    CloseDB(db);
+}
+
+export const Select = (table, select, where="") =>
+{
+    let db = OpenDB();
+
+    let query = db.get("SELECT ? FROM ? WHERE ?", select, table, where, (err) =>{
+        if(err)
+        {
+            console.error(err.message);
+            console.log("Error in select statement");
+        }
+    });
+
+    CloseDB(db);
+    return query;
+}
+
+export const Delete = (table, fields, where) =>
+{
+    let db = OpenDB();
+
+    let query = db.exec("UPDATE ? SET ? WHERE ?", table,fields, where, (err) => {
+        if(err){
+            console.error(err.message);
+            console.log("Error in Delete statement");
+        }
+        else{
+            console.log("Properly Deleted");
+        }
+    });
+
+    CloseDB(db);
+}
+
+
